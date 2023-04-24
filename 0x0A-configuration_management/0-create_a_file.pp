dd3ebebd443104fe348764333e7b76af36785f65
robert5313 <kiptoorobert@zetech.ac.ke>
2023-04-24 14:06:16 -0700

    puppet task

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,9 @@
 # create a file.
+# Using puppet.
 file { '/tmp/school':
-	mode	=> '0744',
-	owner	=> 'www-data',
-	group	=> 'www-data',
-	content => 'I love Puppet',
+  ensure  => present,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
 }
