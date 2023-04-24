9147470d7e3da84bd397d2bf6a174621a97d25f8
Robert Kimaiyo <97959136+robert5313@users.noreply.github.com>
2023-04-24 23:57:56 +0300

    Update 0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,7 @@
-# create a file
+# create a file.
 file { '/tmp/school':
 	mode	=> '0744',
 	owner	=> 'www-data',
 	group	=> 'www-data',
-	content => 'I love Puppet'
+	content => 'I love Puppet',
 }
-
